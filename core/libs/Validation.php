d0549917a8f3e2d567dcbc198ec0eee5290d85dd
Van Neves <vaneves@vaneves.com>
2013-06-18 09:16:39 -0300

    Adição da validação de CPF

--- core/libs/Validation.php
+++ core/libs/Validation.php
@@ -173,9 +173,28 @@ class Validation
 	 * @param	string		$value	CPF (com pontos e hífen)
 	 * @return	boolean		retorna true se o CPF for válido, no contrário retorna false
 	 */
-	public static function cpf($value)
+	public static function cpf($cpf)
 	{
-		
+		$cpf = str_replace('-', '', $cpf);
+				$cpf = str_replace('.', '', $cpf);
+
+		$cpf = str_pad(preg_replace('/[^0-9]/', '', $cpf), 11, '0', STR_PAD_LEFT);
+		if (strlen($cpf) != 11 || $cpf == '00000000000' || $cpf == '11111111111' || $cpf == '22222222222' || $cpf == '33333333333' || $cpf == '44444444444' || $cpf == '55555555555' || $cpf == '66666666666' || $cpf == '77777777777' || $cpf == '88888888888' || $cpf == '99999999999') 
+			return false;
+		else 
+		{ 
+			for ($t = 9; $t < 11; $t++) 
+			{
+				for ($d = 0, $c = 0; $c < $t; $c++) 
+					$d += $cpf{$c} * (($t + 1) - $c);
+
+				$d = ((10 * $d) % 11) % 10;
+
+				if ($cpf{$c} != $d) 
+					return false;
+			}
+			return true;
+		}
 	}
 	
 	/**
